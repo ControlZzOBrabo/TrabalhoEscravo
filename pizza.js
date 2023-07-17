01a22a99ea5c30bb8e3e29aad045ad01ebca97e3
joao <joao_V_rubik@estudante.sesisenai.org.br>
2023-07-17 08:49:50 -0300

    primeiro commit

---
+++ pizza.js
@@ -0,0 +1,13 @@
+data = {
+    datasets: [{
+        data: [10, 20, 30]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Red',
+        'Yellow',
+        'Blue'
+    ]
+};
+ 
